6d9c0adaaecb0ea5ab41611c70862ad180b7528e
Alan Chambers <ajmchambers@users.noreply.github.com>
2020-08-22 12:37:51 +0100

    bleh

---
+++ packages/al-components-react/src/components.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'al-components';
+
+
+
+export const AlCard = /*@__PURE__*/createReactComponent<JSX.AlCard, HTMLAlCardElement>('al-card');
