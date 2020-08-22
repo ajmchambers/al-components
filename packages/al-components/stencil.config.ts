18b6108a368aa10dd511fca78d864e2571d8dd37
Alan Chambers <ajmchambers@users.noreply.github.com>
2020-08-22 12:21:06 +0100

    first commit

---
+++ packages/al-components/stencil.config.ts
@@ -0,0 +1,24 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+export const config: Config = {
+  namespace: 'al-components',
+  taskQueue: 'async',
+  outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'al-components',
+      proxiesFile: '../al-components-react/src/components.ts',
+    }),
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
